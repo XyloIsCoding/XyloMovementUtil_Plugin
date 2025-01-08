d786938cedfbc8e7b16ceeff17b3fe45289f0db6
XyloIsCoding <xylo.is.coding@gmail.com>
2025-01-08 22:40:56 +0100

    cleaned up base class fixing all errors. now stamina and charge should be managed

--- Source/XyloMovementUtil/Private/Movement/Base/XMUBaseMovement.cpp
+++ Source/XyloMovementUtil/Private/Movement/Base/XMUBaseMovement.cpp
@@ -5,25 +5,7 @@
 
 #include "GameFramework/Character.h"
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
- * Network Move Data
- */
 
-void FXMUBaseNetworkMoveData::ClientFillNetworkMoveData(const FSavedMove_Character& ClientMove, ENetworkMoveType MoveType)
-{
-	Super::ClientFillNetworkMoveData(ClientMove, MoveType);
- 
-	Stamina = static_cast<const FXMUSavedMove_Character_Base&>(ClientMove).Stamina;
-}
-
-bool FXMUBaseNetworkMoveData::Serialize(UCharacterMovementComponent& CharacterMovement, FArchive& Ar, UPackageMap* PackageMap, ENetworkMoveType MoveType)
-{
-	Super::Serialize(CharacterMovement, Ar, PackageMap, MoveType);
-    
-	SerializeOptionalValue<float>(Ar.IsSaving(), Ar, Stamina, 0.f);
-	return !Ar.IsError();
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
@@ -37,8 +19,10 @@ void FXMUBaseMoveResponseDataContainer::ServerFillResponseData(
 
 	const UXMUBaseMovement* MoveComp = Cast<UXMUBaseMovement>(&CharacterMovement);
 
-	bStaminaDrained = MoveComp->IsStaminaDrained();
 	Stamina = MoveComp->GetStamina();
+	bStaminaDrained = MoveComp->IsStaminaDrained();
+	Charge = MoveComp->GetCharge();
+	bChargeDrained = MoveComp->IsChargeDrained();
 }
 
 bool FXMUBaseMoveResponseDataContainer::Serialize(UCharacterMovementComponent& CharacterMovement, FArchive& Ar,
@@ -53,8 +37,36 @@ bool FXMUBaseMoveResponseDataContainer::Serialize(UCharacterMovementComponent& C
 	{
 		Ar << Stamina;
 		Ar << bStaminaDrained;
+		Ar << Charge;
+		Ar << bChargeDrained;
 	}
 
+	return !Ar.IsError();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Network Move Data
+ */
+
+void FXMUBaseNetworkMoveData::ClientFillNetworkMoveData(const FSavedMove_Character& ClientMove, ENetworkMoveType MoveType)
+{
+	Super::ClientFillNetworkMoveData(ClientMove, MoveType);
+
+	const FXMUSavedMove_Character_Base BaseClientMove = static_cast<const FXMUSavedMove_Character_Base&>(ClientMove);
+	
+	BaseCompressedFlags = BaseClientMove.GetBaseCompressedFlags();
+	Stamina = BaseClientMove.Stamina;
+	Charge = BaseClientMove.Charge;
+}
+
+bool FXMUBaseNetworkMoveData::Serialize(UCharacterMovementComponent& CharacterMovement, FArchive& Ar, UPackageMap* PackageMap, ENetworkMoveType MoveType)
+{
+	Super::Serialize(CharacterMovement, Ar, PackageMap, MoveType);
+    
+	SerializeOptionalValue<uint8>(Ar.IsSaving(), Ar, CompressedMoveFlags, 0);
+	SerializeOptionalValue<float>(Ar.IsSaving(), Ar, Stamina, 0.f);
+	SerializeOptionalValue<float>(Ar.IsSaving(), Ar, Charge, 0.f);
 	return !Ar.IsError();
 }
 
@@ -63,12 +75,27 @@ bool FXMUBaseMoveResponseDataContainer::Serialize(UCharacterMovementComponent& C
  * Saved Move
  */
 
+void FXMUSavedMove_Character_Base::Clear()
+{
+	Super::Clear();
+	
+	Stamina = 0.f;
+	bStaminaDrained = false;
+	Charge = 0.f;
+	bChargeDrained = false;
+}
+
 bool FXMUSavedMove_Character_Base::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter,
 	float MaxDelta) const
 {
 	const TSharedPtr<FXMUSavedMove_Character_Base>& SavedMove = StaticCastSharedPtr<FXMUSavedMove_Character_Base>(NewMove);
 
 	if (bStaminaDrained != SavedMove->bStaminaDrained)
+	{
+		return false;
+	}
+
+	if (bChargeDrained != SavedMove->bChargeDrained)
 	{
 		return false;
 	}
@@ -87,15 +114,9 @@ void FXMUSavedMove_Character_Base::CombineWith(const FSavedMove_Character* OldMo
 	{
 		MoveComp->SetStamina(SavedOldMove->Stamina);
 		MoveComp->SetStaminaDrained(SavedOldMove->bStaminaDrained);
+		MoveComp->SetCharge(SavedOldMove->Charge);
+		MoveComp->SetChargeDrained(SavedOldMove->bChargeDrained);
 	}
-}
-
-void FXMUSavedMove_Character_Base::Clear()
-{
-	Super::Clear();
-
-	bStaminaDrained = false;
-	Stamina = 0.f;
 }
 
 void FXMUSavedMove_Character_Base::SetInitialPosition(ACharacter* C)
@@ -104,11 +125,19 @@ void FXMUSavedMove_Character_Base::SetInitialPosition(ACharacter* C)
 
 	if (const UXMUBaseMovement* MoveComp = C ? Cast<UXMUBaseMovement>(C->GetCharacterMovement()) : nullptr)
 	{
-		bStaminaDrained = MoveComp->IsStaminaDrained();
 		Stamina = MoveComp->GetStamina();
+		bStaminaDrained = MoveComp->IsStaminaDrained();
+		Charge = MoveComp->GetCharge();
+		bChargeDrained = MoveComp->IsChargeDrained();
 	}
 }
 
+uint8 FXMUSavedMove_Character_Base::GetBaseCompressedFlags() const
+{
+	uint8 Result = 0;
+
+	return Result;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -125,19 +154,212 @@ UXMUBaseMovement::UXMUBaseMovement(const FObjectInitializer& ObjectInitializer)
 	SetNetworkMoveDataContainer(BaseMoveDataContainer);
 }
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Stamina */
+
+void UXMUBaseMovement::SetStamina(float NewStamina)
+{
+	const float PrevStamina = Stamina;
+	Stamina = FMath::Clamp(NewStamina, 0.f, MaxStamina);
+	if (CharacterOwner != nullptr)
+	{
+		if (!FMath::IsNearlyEqual(PrevStamina, Stamina))
+		{
+			OnStaminaChanged(PrevStamina, Stamina);
+		}
+	}
+}
+
+void UXMUBaseMovement::SetMaxStamina(float NewMaxStamina)
+{
+	const float PrevMaxStamina = MaxStamina;
+	MaxStamina = FMath::Max(0.f, NewMaxStamina);
+	if (CharacterOwner != nullptr)
+	{
+		if (!FMath::IsNearlyEqual(PrevMaxStamina, MaxStamina))
+		{
+			OnMaxStaminaChanged(PrevMaxStamina, MaxStamina);
+		}
+	}
+}
+
+void UXMUBaseMovement::SetStaminaDrained(bool bNewValue)
+{
+	const bool bWasStaminaDrained = bStaminaDrained;
+	bStaminaDrained = bNewValue;
+	if (CharacterOwner != nullptr)
+	{
+		if (bWasStaminaDrained != bStaminaDrained)
+		{
+			if (bStaminaDrained)
+			{
+				OnStaminaDrained();
+			}
+			else
+			{
+				OnStaminaDrainRecovered();
+			}
+		}
+	}
+}
+
+void UXMUBaseMovement::DebugStamina() const
+{
+#if !UE_BUILD_SHIPPING
+	if (GEngine)
+	{
+		if (CharacterOwner->HasAuthority())
+		{
+			GEngine->AddOnScreenDebugMessage(38265, 1.f, FColor::Orange, FString::Printf(TEXT("[Authority] Stamina %f    Drained %d"), GetStamina(), IsStaminaDrained()));
+		}
+		else
+		{
+			GEngine->AddOnScreenDebugMessage(38266, 1.f, FColor::Orange, FString::Printf(TEXT("[Autonomous] Stamina %f    Drained %d"), GetStamina(), IsStaminaDrained()));
+		}
+	}
+#endif
+}
+
+void UXMUBaseMovement::OnStaminaChanged(float PrevValue, float NewValue)
+{
+	if (FMath::IsNearlyZero(Stamina))
+	{
+		Stamina = 0.f;
+		if (!bStaminaDrained)
+		{
+			SetStaminaDrained(true);
+		}
+	}
+	// This will need to change if not using MaxStamina for recovery, here is an example (commented out) that uses
+	// 10% instead; to use this, comment out the existing else if statement, and change the 0.1f to the percentage
+	// you want to use (0.1f is 10%)
+	//
+	// else if (bStaminaDrained && Stamina >= MaxStamina * 0.1f)
+	// {
+	// 	SetStaminaDrained(false);
+	// }
+	else if (FMath::IsNearlyEqual(Stamina, MaxStamina))
+	{
+		Stamina = MaxStamina;
+		if (bStaminaDrained)
+		{
+			SetStaminaDrained(false);
+		}
+	}
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Charge */
+
+void UXMUBaseMovement::SetCharge(float NewCharge)
+{
+	const float PrevCharge = Charge;
+	Charge = FMath::Clamp(NewCharge, 0.f, MaxCharge);
+	if (CharacterOwner != nullptr)
+	{
+		if (!FMath::IsNearlyEqual(PrevCharge, Charge))
+		{
+			OnChargeChanged(PrevCharge, Charge);
+		}
+	}
+}
+
+void UXMUBaseMovement::SetMaxCharge(float NewMaxCharge)
+{
+	const float PrevMaxCharge = MaxCharge;
+	MaxCharge = FMath::Max(0.f, NewMaxCharge);
+	if (CharacterOwner != nullptr)
+	{
+		if (!FMath::IsNearlyEqual(PrevMaxCharge, MaxCharge))
+		{
+			OnMaxChargeChanged(PrevMaxCharge, MaxCharge);
+		}
+	}
+}
+
+void UXMUBaseMovement::SetChargeDrained(bool bNewValue)
+{
+	const bool bWasChargeDrained = bChargeDrained;
+	bChargeDrained = bNewValue;
+	if (CharacterOwner != nullptr)
+	{
+		if (bWasChargeDrained != bChargeDrained)
+		{
+			if (bChargeDrained)
+			{
+				OnChargeDrained();
+			}
+			else
+			{
+				OnChargeDrainRecovered();
+			}
+		}
+	}
+}
+
+void UXMUBaseMovement::DebugCharge() const
+{
+#if !UE_BUILD_SHIPPING
+	if (GEngine)
+	{
+		if (CharacterOwner->HasAuthority())
+		{
+			GEngine->AddOnScreenDebugMessage(38265, 1.f, FColor::Orange, FString::Printf(TEXT("[Authority] Charge %f    Drained %d"), GetCharge(), IsChargeDrained()));
+		}
+		else
+		{
+			GEngine->AddOnScreenDebugMessage(38266, 1.f, FColor::Orange, FString::Printf(TEXT("[Autonomous] Charge %f    Drained %d"), GetCharge(), IsChargeDrained()));
+		}
+	}
+#endif
+}
+
+void UXMUBaseMovement::OnChargeChanged(float PrevValue, float NewValue)
+{
+	if (FMath::IsNearlyZero(Charge))
+	{
+		Charge = 0.f;
+		if (!bChargeDrained)
+		{
+			SetChargeDrained(true);
+		}
+	}
+	// This will need to change if not using MaxCharge for recovery, here is an example (commented out) that uses
+	// 10% instead; to use this, comment out the existing else if statement, and change the 0.1f to the percentage
+	// you want to use (0.1f is 10%)
+	//
+	// else if (bChargeDrained && Charge >= MaxCharge * 0.1f)
+	// {
+	// 	SetChargeDrained(false);
+	// }
+	else if (FMath::IsNearlyEqual(Charge, MaxCharge))
+	{
+		Charge = MaxCharge;
+		if (bChargeDrained)
+		{
+			SetChargeDrained(false);
+		}
+	}
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Networking stuff */
 
 void UXMUBaseMovement::OnClientCorrectionReceived(FNetworkPredictionData_Client_Character& ClientData, float TimeStamp,
-                                                  FVector NewLocation, FVector NewVelocity, UPrimitiveComponent* NewBase, FName NewBaseBoneName, bool bHasBase,
-                                                  bool bBaseRelativePosition, uint8 ServerMovementMode, FVector ServerGravityDirection)
+	FVector NewLocation, FVector NewVelocity, UPrimitiveComponent* NewBase, FName NewBaseBoneName, bool bHasBase,
+	bool bBaseRelativePosition, uint8 ServerMovementMode, FVector ServerGravityDirection)
 {
 	// ClientHandleMoveResponse() ➜ ClientAdjustPosition_Implementation() ➜ OnClientCorrectionReceived()
-	const FXMUBaseMoveResponseDataContainer& StaminaMoveResponse = static_cast<const FXMUBaseMoveResponseDataContainer&>(GetMoveResponseDataContainer());
+	const FXMUBaseMoveResponseDataContainer& BaseMoveResponse = static_cast<const FXMUBaseMoveResponseDataContainer&>(GetMoveResponseDataContainer());
 	
-	SetStamina(StaminaMoveResponse.Stamina);
-	SetStaminaDrained(StaminaMoveResponse.bStaminaDrained);
+	SetStamina(BaseMoveResponse.Stamina);
+	SetStaminaDrained(BaseMoveResponse.bStaminaDrained);
+	SetCharge(BaseMoveResponse.Charge);
+	SetChargeDrained(BaseMoveResponse.bChargeDrained);
 
 	Super::OnClientCorrectionReceived(ClientData, TimeStamp, NewLocation, NewVelocity, NewBase, NewBaseBoneName,
 	bHasBase, bBaseRelativePosition, ServerMovementMode, ServerGravityDirection);
@@ -153,10 +375,15 @@ bool UXMUBaseMovement::ServerCheckClientError(float ClientTimeStamp, float Delta
 	// This will trigger a client correction if the Stamina value in the Client differs NetworkStaminaCorrectionThreshold (2.f default) units from the one in the server
 	// Desyncs can happen if we set the Stamina directly in Gameplay code (ie: GAS)
     const FXMUBaseNetworkMoveData* CurrentMoveData = static_cast<const FXMUBaseNetworkMoveData*>(GetCurrentNetworkMoveData());
-    if (!FMath::IsNearlyEqual(CurrentMoveData->Stamina, Stamina, NetworkStaminaCorrectionThreshold))
+
+	if (!FMath::IsNearlyEqual(CurrentMoveData->Stamina, Stamina, NetworkStaminaCorrectionThreshold))
     {
         return true;
     }
+	if (!FMath::IsNearlyEqual(CurrentMoveData->Charge, Charge, NetworkChargeCorrectionThreshold))
+	{
+		return true;
+	}
     
     return false;
 }
--- Source/XyloMovementUtil/Public/Movement/Base/XMUBaseMovement.h
+++ Source/XyloMovementUtil/Public/Movement/Base/XMUBaseMovement.h
@@ -6,21 +6,40 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "XMUBaseMovement.generated.h"
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Move Response Data: What we receive from server
+ *	call UCharacterMovementComponent::SetMoveResponseDataContainer in cmc constructor to use this class
+ */
+
+struct XYLOMOVEMENTUTIL_API FXMUBaseMoveResponseDataContainer : FCharacterMoveResponseDataContainer
+{
+	using Super = FCharacterMoveResponseDataContainer;
+	
+	virtual void ServerFillResponseData(const UCharacterMovementComponent& CharacterMovement, const FClientAdjustment& PendingAdjustment) override;
+	virtual bool Serialize(UCharacterMovementComponent& CharacterMovement, FArchive& Ar, UPackageMap* PackageMap) override;
+
+	float Stamina;
+	bool bStaminaDrained;
+	float Charge;
+	bool bChargeDrained;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
  * Network Move Data: What we send to server
  */
 
-struct EXOMOVEMENTS_API FXMUBaseNetworkMoveData : public FCharacterNetworkMoveData
+struct XYLOMOVEMENTUTIL_API FXMUBaseNetworkMoveData : public FCharacterNetworkMoveData
 {
 public:
 	typedef FCharacterNetworkMoveData Super;
  
 	FXMUBaseNetworkMoveData()
-		: Stamina(0)
+		: BaseCompressedFlags(0)
+		, Stamina(0)
+		, Charge(0)
 	{
-
 	}
  
 	virtual void ClientFillNetworkMoveData(const FSavedMove_Character& ClientMove, ENetworkMoveType MoveType) override;
@@ -30,13 +49,19 @@ public:
 	 * Custom Data
 	 */
 
-	//uint8 BaseCompressedFlags;
+	uint8 BaseCompressedFlags; // generated using FXMUSavedMove_Character_Base::GetBaseCompressedFlags
 	float Stamina;
-	//float Charge;
+	float Charge;
 	
 };
 
-struct EXOMOVEMENTS_API FXMUBaseNetworkMoveDataContainer : public FCharacterNetworkMoveDataContainer
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Network Move Data Container: Wraps Network Move Data to send to server
+ *	call UCharacterMovementComponent::SetNetworkMoveDataContainer in cmc constructor to use this class
+ */
+
+struct XYLOMOVEMENTUTIL_API FXMUBaseNetworkMoveDataContainer : public FCharacterNetworkMoveDataContainer
 {
 public:
 	typedef FCharacterNetworkMoveDataContainer Super;
@@ -54,54 +79,59 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
- * Move Response Data: What we receive from server
- */
-
-struct EXOMOVEMENTS_API FXMUBaseMoveResponseDataContainer : FCharacterMoveResponseDataContainer
-{
-	using Super = FCharacterMoveResponseDataContainer;
-	
-	virtual void ServerFillResponseData(const UCharacterMovementComponent& CharacterMovement, const FClientAdjustment& PendingAdjustment) override;
-	virtual bool Serialize(UCharacterMovementComponent& CharacterMovement, FArchive& Ar, UPackageMap* PackageMap) override;
-
-	float Stamina;
-	bool bStaminaDrained;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
  * Saved Move: passed to FCharacterNetworkMoveData to be sent to server when necessary
  */
 
-class EXOMOVEMENTS_API FXMUSavedMove_Character_Base : public FSavedMove_Character
+class XYLOMOVEMENTUTIL_API FXMUSavedMove_Character_Base : public FSavedMove_Character
 {
 	using Super = FSavedMove_Character;
 	
 public:
 	FXMUSavedMove_Character_Base()
-		: bStaminaDrained(0)
-		, Stamina(0)
+		: Stamina(0)
+		, bStaminaDrained(0)
+		, Charge(0)
+		, bChargeDrained(0)
 	{
 	}
 
 	virtual ~FXMUSavedMove_Character_Base() override
 	{}
-
-	uint32 bStaminaDrained : 1;
+	
 	float Stamina;
+	uint32 bStaminaDrained : 1;
+	float Charge;
+	uint32 bChargeDrained : 1;
 
+	virtual void Clear() override;
 	virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
 	virtual void CombineWith(const FSavedMove_Character* OldMove, ACharacter* InCharacter, APlayerController* PC, const FVector& OldStartLocation) override;
-	virtual void Clear() override;
 	virtual void SetInitialPosition(ACharacter* C) override;
+	/** Generate a compressed flags based on saved moved variables */
+	virtual uint8 GetBaseCompressedFlags() const;
+
+	// Bit masks used by GetCompressedFlags() to encode movement information.
+	enum BaseCompressedFlags
+	{
+		FLAG_Reserved_1		= 0x01,
+		FLAG_Reserved_2		= 0x02,
+		FLAG_Reserved_3		= 0x04,
+		FLAG_Reserved_4		= 0x08,
+		FLAG_Custom_0		= 0x10,
+		FLAG_Custom_1		= 0x20,
+		FLAG_Custom_2		= 0x40,
+		FLAG_Custom_3		= 0x80,
+	};
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
- * Network Prediction Data
+ * Network Prediction Data: manages saved moves
+ *	it is returned by UXMUBaseMovement::GetPredictionData_Client, this is the way we set the SavedMove class
+ *	for this component
  */
 
-class EXOMOVEMENTS_API FXMUNetworkPredictionData_Client_Character_Base : public FNetworkPredictionData_Client_Character
+class XYLOMOVEMENTUTIL_API FXMUNetworkPredictionData_Client_Character_Base : public FNetworkPredictionData_Client_Character
 {
 	using Super = FNetworkPredictionData_Client_Character;
 
@@ -141,6 +171,84 @@ class XYLOMOVEMENTUTIL_API UXMUBaseMovement : public UCharacterMovementComponent
 public:
 	UXMUBaseMovement(const FObjectInitializer& ObjectInitializer);
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+	/* Stamina */
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	float GetStamina() const { return Stamina; }
+	UFUNCTION(BlueprintCallable)
+	float GetMaxStamina() const { return MaxStamina; }
+	bool IsStaminaDrained() const { return bStaminaDrained; }
+	void SetStamina(float NewStamina);
+	void SetMaxStamina(float NewMaxStamina);
+	void SetStaminaDrained(bool bNewValue);
+	void DebugStamina() const;
+protected:
+    /*
+     * Drain state entry and exit is handled here. Drain state is used to prevent rapid re-entry of sprinting or other
+     * such abilities before sufficient stamina has regenerated. However, in the default implementation, 100%
+     * stamina must be regenerated. Consider overriding this, check the implementation's comment for more information.
+     */
+    virtual void OnStaminaChanged(float PrevValue, float NewValue);
+
+    virtual void OnMaxStaminaChanged(float PrevValue, float NewValue) {}
+    virtual void OnStaminaDrained() {}
+    virtual void OnStaminaDrainRecovered() {}
+protected:
+	/** THIS SHOULD ONLY BE MODIFIED IN DERIVED CLASSES FROM OnStaminaChanged AND NOWHERE ELSE */
+	UPROPERTY()
+	float Stamina;
+private:
+	UPROPERTY()
+	float MaxStamina;
+	UPROPERTY()
+	bool bStaminaDrained;
+	/** Maximum stamina difference that is allowed between client and server before a correction occurs. */
+	UPROPERTY(Category="Character Movement (Networking)", EditDefaultsOnly, meta=(ClampMin="0.0", UIMin="0.0"))
+	float NetworkStaminaCorrectionThreshold;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+	/* Charge */
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	float GetCharge() const { return Charge; }
+	UFUNCTION(BlueprintCallable)
+	float GetMaxCharge() const { return MaxCharge; }
+	bool IsChargeDrained() const { return bChargeDrained; }
+	void SetCharge(float NewCharge);
+	void SetMaxCharge(float NewMaxCharge);
+	void SetChargeDrained(bool bNewValue);
+	void DebugCharge() const;
+protected:
+	/*
+	 * Drain state entry and exit is handled here. Drain state is used to prevent rapid re-entry of sprinting or other
+	 * such abilities before sufficient Charge has regenerated. However, in the default implementation, 100%
+	 * Charge must be regenerated. Consider overriding this, check the implementation's comment for more information.
+	 */
+	virtual void OnChargeChanged(float PrevValue, float NewValue);
+
+	virtual void OnMaxChargeChanged(float PrevValue, float NewValue) {}
+	virtual void OnChargeDrained() {}
+	virtual void OnChargeDrainRecovered() {}
+protected:
+	/** THIS SHOULD ONLY BE MODIFIED IN DERIVED CLASSES FROM OnChargeChanged AND NOWHERE ELSE */
+	UPROPERTY()
+	float Charge;
+private:
+	UPROPERTY()
+	float MaxCharge;
+	UPROPERTY()
+	bool bChargeDrained;
+	/** Maximum stamina difference that is allowed between client and server before a correction occurs. */
+	UPROPERTY(Category="Character Movement (Networking)", EditDefaultsOnly, meta=(ClampMin="0.0", UIMin="0.0"))
+	float NetworkChargeCorrectionThreshold;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+	
 /*--------------------------------------------------------------------------------------------------------------------*/
 	/* Networking stuff */
 
